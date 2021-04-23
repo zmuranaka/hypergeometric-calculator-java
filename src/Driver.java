dc351674dabb8ad9c0315fda5b43f9398c49bef4
zmuranaka <zmuranaka@gmail.com>
2021-04-23 11:26:38 -0600

    Large overhaul to match my C++ version better

--- src/Driver.java
+++ src/Driver.java
@@ -9,28 +9,13 @@ public class Driver
     public static void main(String[] args)
     {
         // The next four statements use static methods from check.java to get valid input
-        long popSize = check.validPopSize();
-        long popSuccesses = check.validPopSuccesses();
-        long sampleSize = check.validSampleSize();
-        long desiredSuccesses = check.validDesiredSucccesses();
+        long popSize = check.validData("Population size: ");
+        long popSuccesses = check.validData("Population successes: ");
+        long sampleSize = check.validData("Sample size: ");
+        long desiredSuccesses = check.validData("Desired successes: ");
 
-        Deck orGreaterDeck = new Deck(popSize, popSuccesses, sampleSize, desiredSuccesses); // Constructs a Deck object
-        Deck orLessDeck = new Deck(orGreaterDeck); // Constructs a copy of the above Deck
-        
-        // The next next six statements calculate probabilities for the Decks
-        double orGreaterExactChance = orGreaterDeck.probability(orGreaterDeck.getdpopSize(), orGreaterDeck.getdpopSuccesses(), orGreaterDeck.getddesiredSuccesses(), orGreaterDeck.getdpopFailures(), orGreaterDeck.getdsampleFailures());
-        double orLessExactChance = orLessDeck.probability(orLessDeck.getdpopSize(), orLessDeck.getdpopSuccesses(), orLessDeck.getddesiredSuccesses(), orLessDeck.getdpopFailures(), orLessDeck.getdsampleFailures());
-        orGreaterExactChance *= orGreaterDeck.getcombination(); // Chance of getting exactly desired successes
-        double orGreaterInclusiveChance = orGreaterDeck.orGreater(orGreaterExactChance); // Chance of getting desired successes or greater
-        orLessExactChance *= orLessDeck.getcombination(); // Chance of getting exactly desired successes
-        double orLessInclusiveChance = orLessDeck.orLess(orLessExactChance); // Chance of getting desired successes or less
-        
-        // The next five statements print out the probabilities
-        System.out.println("Chance of exactly desired successes: " + orGreaterExactChance); // orGreaterExactChance and orLessExactChance should be equal so this could also be orLessExactChance
-        System.out.println("Chance of less than desired successes: " + (orLessInclusiveChance - orLessExactChance));
-        System.out.println("Chance of desired successes or less: " + orLessInclusiveChance);
-        System.out.println("Chance of greater than desired successes: " + (orGreaterInclusiveChance - orGreaterExactChance));
-        System.out.println("Chance of desired successes or greater: " + orGreaterInclusiveChance);
+        // Construct the Deck object and print the results
+        Deck myDeck = new Deck(popSize, popSuccesses, sampleSize, desiredSuccesses);
+        myDeck.print();
     }
-
 }
--- src/check.java
+++ src/check.java
@@ -1,116 +1,31 @@
 /*
  * File: check.java
  * Zachary Muranaka
- * Contains methods that the Driver uses to check if the user entered valid longs
+ * Contains a static method that the Driver uses to make sure the user enters a valid long
  */
 
 import java.util.Scanner;
 
 public class check
 {
-    static Scanner input = new Scanner(System.in);
+    static Scanner reader = new Scanner(System.in);
 
-    public static long validPopSize()
+    public static long validData(final String prompt)
     {
-        System.out.print("Population size: ");
-        String spopSize = input.nextLine(); // Reads the user input as a String
-        long lpopSize = popSize(spopSize);
-        if(lpopSize == -1)
-            return validPopSize(); // The method will loop if the user entered an invalid long
-        return lpopSize;
-    }
-
-   public static long validPopSuccesses()
-   {
-        System.out.print("Population successes: ");
-        String spopSuccesses = input.nextLine(); // Reads the user input as a String
-        long lpopSuccesses = popSuccesses(spopSuccesses);
-        if(lpopSuccesses == -1)
-            return validPopSuccesses(); // The method will loop if the user entered an invalid long
-        return lpopSuccesses;
-   }
-
-   public static long validSampleSize()
-   {
-        System.out.print("Sample Size: ");
-        String ssampleSize = input.nextLine(); // Reads the user input as a String
-        long lsampleSize = sampleSize(ssampleSize);
-        if(lsampleSize == -1)
-            return validSampleSize(); // The method will loop if the user entered an invalid long
-        return lsampleSize;
-   }
-
-   public static long validDesiredSucccesses()
-   {
-        System.out.print("Desired Successes: ");
-        String sdesiredSuccesses = input.nextLine(); // Reads the user input as a String
-        long ldesiredSuccesses = desiredSuccesses(sdesiredSuccesses);
-        if(ldesiredSuccesses == -1)
-            return validDesiredSucccesses(); // The method will loop if the user entered an invalid long
-        return ldesiredSuccesses;
-   }
-
-    private static long popSize(String input)
-    {
-        // Tries to convert the String the user entered to a long
+        System.out.print(prompt);
+        String userInput = reader.nextLine(); // Reads the user input as a String
+        
+        // Try to convert the user input to a long and return it
         try
         {
-            long popSize = Long.parseLong(input);
-            return popSize;
+            long potentialData = Long.parseLong(userInput);
+            return potentialData;
         }
-        // The user entered something that is not a long
-        catch(NumberFormatException nException)
+        // Catches cases when the user's input could not be converted to a long
+        catch (NumberFormatException nException)
         {
-            System.out.println("Population size should be an integer. Try again.");
-            return -1; // -1 indicates that there was an error
-        }
-    }
-
-    private static long popSuccesses(String input)
-    {
-        // Tries to convert the String the user entered to a long
-        try
-        {
-            long popSuccesses = Long.parseLong(input);
-            return popSuccesses;
-        }
-        // The user entered something that is not a long
-        catch(NumberFormatException nException)
-        {
-            System.out.println("Population successes should be an integer. Try again.");
-            return -1; // -1 indicates that there was an error
-        }
-    }
-
-    private static long sampleSize(String input)
-    {
-        // Tries to convert the String the user entered to a long
-        try
-        {
-            long sampleSize = Long.parseLong(input);
-            return sampleSize;
-        }
-        // The user entered something that is not a long
-        catch(NumberFormatException nException)
-        {
-            System.out.println("Sample size should be an integer. Try again.");
-            return -1; // -1 indicates that there was an error
-        }
-    }
-
-    private static long desiredSuccesses(String input)
-    {
-        // Tries to convert the String the user entered to a long
-        try
-        {
-            long desiredSuccesses = Long.parseLong(input);
-            return desiredSuccesses;
-        }
-        // The user entered something that is not a long
-        catch(NumberFormatException nException)
-        {
-            System.out.println("Desired Successes should be an integer. Try again.");
-            return -1; // -1 indicates that there was an error
+            System.out.println(prompt.substring(0, prompt.length() - 2) + " should be an integer. Try again.");
+            return validData(prompt);
         }
     }
 }
